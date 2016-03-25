9970d56897e7091e3ce218a1d5b47f308e71aa62
Eugene Chiong <eugenecys@gmail.com>
2016-03-25 00:16:32 -0400

    Added material control for component class

--- Assets/Scripts/Component.cs
+++ Assets/Scripts/Component.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public abstract class Component : MonoBehaviour {
 
+    AssetManager assetManager;
+
     public enum State
     {
         Deployed,
@@ -11,17 +13,20 @@ public abstract class Component : MonoBehaviour {
         Unconnectable
     }
 
+    public Material defaultMaterial;
     public State state;
-
+    public Renderer rend;
     List<Component> connectedComponents;
     public bool movable { get; protected set; }
     public Collider collider;
     public Collider trigger;
     public Rigidbody rb;
-
+    
+    private Collider otherCollider;
 
 	// Use this for initialization
 	void Awake () {
+        assetManager = AssetManager.Instance;
         connectedComponents = new List<Component>();
         setState(State.Unconnectable);
         init();
@@ -29,7 +34,7 @@ public abstract class Component : MonoBehaviour {
 
     public bool connectable()
     {
-        return false;
+        return otherCollider != null;
     }
 
     public void connect()
@@ -43,12 +48,15 @@ public abstract class Component : MonoBehaviour {
         {
             case State.Deployed:
                 state = State.Deployed;
+                rend.material = defaultMaterial;
                 break;
             case State.Connectable:
                 state = State.Connectable;
+                rend.material = assetManager.connectableMaterial;
                 break;
             case State.Unconnectable:
                 state = State.Unconnectable;
+                rend.material = assetManager.unconnectableMaterial;
                 break;
         }
     }
@@ -60,7 +68,15 @@ public abstract class Component : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("COOL");
+        
+        otherCollider = other;
+        setState(State.Connectable);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        otherCollider = null;
+        setState(State.Unconnectable);
     }
 
     protected abstract void update();
