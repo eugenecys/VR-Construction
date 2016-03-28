86355d021cc47a53b69536219f50762763bf5ce8
Eugene Chiong <eugenecys@gmail.com>
2016-03-28 18:20:40 -0400

    Added weapon launching

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,11 +3,22 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour {
 
+    EventManager eventManager;
+
     public GameObject ammo;
+    public float ammoVelocity;
 
     public void fire()
     {
-        Debug.Log("Fire!");
+        GameObject sObj = Object.Instantiate(ammo, transform.position, Quaternion.identity) as GameObject;
+        Rigidbody rb = sObj.GetComponent<Rigidbody>();
+        rb.velocity = ammoVelocity * transform.forward;
+        eventManager.addEvent(() => Destroy(sObj), 2f, true);
+    }
+
+    void Awake()
+    {
+        eventManager = EventManager.Instance;
     }
 
 	// Use this for initialization
