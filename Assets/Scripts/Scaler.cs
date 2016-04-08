961a82409cc82a0025580f24e8e3cac9e12ac22f
Eugene Chiong <eugenecys@gmail.com>
2016-04-08 14:32:59 -0400

    Created a scaling class for scaling objects

---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scaler : MonoBehaviour {
+
+    public enum Direction
+    {
+        X,
+        Y,
+        Z
+    }
+
+    void Awake()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
