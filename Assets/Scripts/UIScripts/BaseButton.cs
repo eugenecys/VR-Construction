3eba6a64a13859308609eaf5606881815ec579e5
krroy03 <rikkyroy@gmail.com>
2016-04-19 11:05:55 -0400

    changed lighting when bases selected
    Signed-off-by: krroy03 <rikkyroy@gmail.com>

---
+++ Assets/Scripts/UIScripts/BaseButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BaseButton : MonoBehaviour, Interactable {
+
+	public Button.ButtonClickedEvent btnFunction;
+	public Light baseLight; 
+	private Color originalColor; 
+	private bool highlighted = false;
+	// Use this for initialization
+	void Start () {
+		originalColor = baseLight.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay (Collider col)
+	{
+		if (!highlighted) {
+			highlight ();
+		}
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		unhighlight ();
+	}
+
+	public void highlight ()
+	{	baseLight.color = Color.blue;
+		highlighted = true;
+	}
+
+	public void unhighlight ()
+	{
+		baseLight.color = originalColor;
+		highlighted = false;
+	}
+}
+	
