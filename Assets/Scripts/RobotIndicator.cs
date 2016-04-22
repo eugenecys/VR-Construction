fcd43e28acf5594fdbe08333c43dd9d8abcd4874
demo <demo>
2016-04-21 23:05:19 -0400

    not complete version of indicator

--- Assets/Scripts/RobotIndicator.cs
+++ Assets/Scripts/RobotIndicator.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class RobotIndicator : MonoBehaviour {
     private GameManager gameManager;
-    private Robot robot;
     private GameObject m_indicator;
+	private bool isIndicated;
+	private MeshRenderer mesh;
+	private Transform robotTran;
 
+	public GameObject robot;
     public GameObject indicator;
     public GameObject headSet;
     public float rayLen;
@@ -13,42 +16,57 @@ public class RobotIndicator : MonoBehaviour {
     // Use this for initialization
     void Awake () {
         gameManager = GameManager.Instance;
-        robot = Robot.Instance;
     }
 	void Start () {
-        m_indicator = Instantiate(indicator, robot.transform.position + new Vector3(0, height, 0), Quaternion.identity) as GameObject;
+		isIndicated = false;
+		robotTran = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		LocateIndicator ();
         SetIndicatorInvisiblity();
 	}
 
+	void LocateIndicator(){
+		if (!isIndicated && gameManager.state == GameManager.GameState.Build) {
+			robotTran = robot.transform.Find ("BaseBodyContainer");
+			if (robotTran != null) {
+				isIndicated = true;
+				m_indicator = Instantiate(indicator, robotTran.position + new Vector3(0, height, 0), Quaternion.identity) as GameObject;
+				m_indicator.transform.SetParent (robotTran);
+				mesh = m_indicator.GetComponent<MeshRenderer> ();
+			} 
+		}
+	}
+
     void SetIndicatorInvisiblity() {
-        if (gameManager.state == GameManager.GameState.Play)
-        {
-            IndicatorActivate();
-        }
-        else {
-            m_indicator.SetActive(false);
-        }
+		if (isIndicated) {
+			if (gameManager.state == GameManager.GameState.Play)
+			{
+				IndicatorActivate();
+			}
+			else {
+				mesh.enabled = false;
+			}
+		}
     }
 
     void IndicatorActivate() {
-        Ray ray = new Ray(robot.transform.position, headSet.transform.position);
+		Ray ray = new Ray(robotTran.transform.position, headSet.transform.position);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, rayLen))
         {
             if (hit.transform.CompareTag("building"))
             {
-                m_indicator.SetActive(true);
+				mesh.enabled = true;
             }
             else {
-                m_indicator.SetActive(false);
+				mesh.enabled = false;
             }
         }
         else {
-            m_indicator.SetActive(false);
+			mesh.enabled = false;
         }
     }
 }
