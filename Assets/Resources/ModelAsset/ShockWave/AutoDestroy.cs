ba532ad3bdb04384a859752c0a2fee6aa344683c
firekarl <firekarlxshan@gmail.com>
2016-04-24 23:08:31 -0400

    add big bang

---
+++ Assets/Resources/ModelAsset/ShockWave/AutoDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour {
+    ParticleSystem part;
+	// Use this for initialization
+	void Start () {
+        part = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (part && part.isStopped)
+            Destroy(gameObject);
+	}
+}
---
+++ Assets/Resources/ModelAsset/ShockWave/ShockWave.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShockWave : MonoBehaviour {
+    Rigidbody rg;
+    SphereCollider collider;
+    float maxRadius = 50f;
+    float duration = 10f;
+    void Start()
+    {
+        collider = GetComponent<SphereCollider>();
+        collider.radius = 0;
+        rg = GetComponent<Rigidbody>();
+        StartCoroutine(Expand());
+    }
+
+    IEnumerator Expand()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime / duration;
+            collider.radius = maxRadius * t;
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "building")
+            col.gameObject.SendMessage("GiveAttack");
+    }
+}
